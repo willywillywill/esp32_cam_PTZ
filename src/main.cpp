30c90f8334f7734ed390ca62f6a8259ddc83bcf3
willywillywill <43361233asd@gmail.com>
2023-02-09 02:54:56 +0800

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <esp32_camera.h>
+#include <esp_https_server.h>
+#include <Wire.h>
+#include <BH1750.h>
+
+void setup() {
+
+}
+
+void loop() {
+
+}
