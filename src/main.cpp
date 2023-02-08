92a514fe62fb9c4d8bb70ea1ba7e24a3173ab60e
willywillywill <43361233asd@gmail.com>
2023-02-09 03:15:40 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
-#include <Arduino.h>
-#include <WiFi.h>
-#include <esp32_camera.h>
-#include <esp_https_server.h>
-#include <Wire.h>
-#include <BH1750.h>
+#include "Arduino.h"
+#include "WiFi.h"
+#include "esp_https_server.h"
+#include "Wire.h"
+
+#include "esp_camera.h"
+#include "BH1750.h"
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
 void setup() {
 
